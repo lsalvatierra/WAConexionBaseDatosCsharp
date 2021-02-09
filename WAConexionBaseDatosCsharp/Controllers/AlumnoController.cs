ec12bed60abda7b3770cfec4aa70f291481723a6
Luis Angel Salvatierra Aquino <luisangel.salvat@gmail.com>
2021-02-09 15:25:23 -0500

    Agregar archivos de proyecto.

---
+++ WAConexionBaseDatosCsharp/Controllers/AlumnoController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WAConexionBaseDatosCsharp.Models.DaoNotas;
+using WAConexionBaseDatosCsharp.Models;
+
+
+namespace WAConexionBaseDatosCsharp.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            ViewBag.lstalumno = new AlumnoDAO().Listar();
+            return View();
+        }
+
+        public ActionResult Nuevo(String idalumno)
+        {
+            if (idalumno != "") {
+                ViewBag.alumno = new AlumnoDAO().Obtener(idalumno);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(String txtnombre, String txtapellido, String txtespecialidad, String txtprocedencia, String idalumno) {
+            Boolean rpta = true;
+            Alumno objalumno = new Alumno();
+            objalumno.NomAlumno = txtnombre;
+            objalumno.ApeAlumno = txtapellido;
+            objalumno.Idesp = txtespecialidad;
+            objalumno.PROCE = txtprocedencia;
+            objalumno.IdAlumno = idalumno;
+            if (idalumno == "")
+            {
+                rpta = new AlumnoDAO().Registrar(objalumno);
+            }
+            else {
+                rpta = new AlumnoDAO().Actualizar(objalumno);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Obtener(String idalumno)
+        {
+            if (idalumno != "")
+            {
+                ViewBag.alumno = new AlumnoDAO().Obtener(idalumno);
+            }
+            return View();
+        }
+        public ActionResult Eliminar(String idalumno)
+        {
+            Boolean rpta = new AlumnoDAO().Eliminar(idalumno);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ WAConexionBaseDatosCsharp/Models/DaoNotas/AlumnoDAO.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAConexionBaseDatosCsharp.Models.DaoNotas
+{
+    public class AlumnoDAO
+    {
+
+        public List<Alumno> Listar()
+        {
+            List<Alumno> lista = new List<Alumno>();
+            using (var data = new bdnotasEntities())
+            {
+                lista = data.Alumno.ToList();
+
+            }
+            return lista;
+        }
+
+        public Alumno Obtener(String idalumno) {
+           Alumno obj = new Alumno();
+            using (var data = new bdnotasEntities())
+            {
+                obj = data.Alumno.Where(q => q.IdAlumno == idalumno).FirstOrDefault();
+            }
+            return obj;
+        }
+
+        public Boolean Registrar(Alumno objalumno)
+        {
+            bool exito = true;
+            try
+            {
+                using (var data = new bdnotasEntities())
+                {
+                    exito = data.sp_MantRegistrarAlumno(objalumno.NomAlumno, objalumno.ApeAlumno,
+                        objalumno.Idesp, objalumno.PROCE) > 0 ? true : false;
+          
+                }
+            }
+            catch (Exception ex)
+            {
+                exito = false;
+
+            }
+            return exito;
+        }
+
+        public Boolean Actualizar(Alumno objalumno)
+        {
+            bool exito = true;
+            try
+            {
+                using (var data = new bdnotasEntities())
+                {
+                    using (var dbContextTransaction = data.Database.BeginTransaction())
+                    {
+                        Alumno alumnoactual = data.Alumno.Where(q => q.IdAlumno == objalumno.IdAlumno).FirstOrDefault();
+                        alumnoactual.NomAlumno = objalumno.NomAlumno;
+                        alumnoactual.ApeAlumno = objalumno.ApeAlumno;
+                        alumnoactual.Idesp = objalumno.Idesp;
+                        alumnoactual.PROCE = objalumno.PROCE;                        
+                        data.SaveChanges();
+                        dbContextTransaction.Commit();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                exito = false;
+
+            }
+            return exito;
+        }
+
+        public Boolean Eliminar(String idalumno) {
+            bool exito = true;
+            try
+            {
+                using (var data = new bdnotasEntities())
+                {
+                    exito = data.sp_MantEliminarAlumno(idalumno) > 0 ? true : false;
+                }
+            }
+            catch (Exception ex)
+            {
+                exito = false;
+
+            }
+            return exito;
+        }
+    }
+}
